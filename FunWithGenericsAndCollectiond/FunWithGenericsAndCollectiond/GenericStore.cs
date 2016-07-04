58e77b41402a0481e1f9fe894bb114ce80785358
Vadim <amdarisdev@gmail.com>
2016-07-04 16:02:59 +0300

    create GenericCollection class

---
+++ FunWithGenericsAndCollectiond/FunWithGenericsAndCollectiond/GenericStore.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunWithGenericsAndCollectiond
+{
+    public class GenericStore<T>
+    {
+        private readonly int _maxSize = 50;
+        private readonly int _currentSize;
+        //private int _currentIndex = 0;
+        private T[] _item;
+
+        public GenericStore(int size)
+        {
+            _currentSize = size;
+            _item = new T[_currentSize];
+        }
+
+        internal T Get(int index)
+        {
+            if (index < _maxSize && _item[index] != null)
+                return _item[index];
+            throw new IndexOutOfRangeException();
+        }
+
+        internal void Set(int index, T value)
+        {
+            if (index < _currentSize)
+            {
+                _item[index] = value;
+            }
+            else
+            {
+                throw new IndexOutOfRangeException();
+            }
+        }
+
+        internal void ChangeValues(int index1, int index2)
+        {
+            if (index1 < _currentSize && index2 < _currentSize)
+            {
+                T temp = _item[index1];
+                _item[index1] = _item[index2];
+                _item[index2] = temp;
+            }
+            else
+            {
+                throw new IndexOutOfRangeException();
+            }
+        }
+    }
+}
--- FunWithGenericsAndCollectiond/FunWithGenericsAndCollectiond/Program.cs
+++ FunWithGenericsAndCollectiond/FunWithGenericsAndCollectiond/Program.cs
@@ -10,6 +10,22 @@ namespace FunWithGenericsAndCollectiond
     {
         static void Main(string[] args)
         {
+            GenericStore<int> store = new GenericStore<int>(20);
+            store.Set(0, 5);
+            store.Set(1, 15);
+            store.Set(2, 25);
+            store.Set(3, 35);
+            store.Set(4, 45);
+            store.Set(5, 55);
+
+            store.ChangeValues(0, 5);
+
+            Console.WriteLine(store.Get(0));
+            Console.WriteLine(store.Get(5));
+
+            Console.ReadLine();
         }
+
+
     }
 }
